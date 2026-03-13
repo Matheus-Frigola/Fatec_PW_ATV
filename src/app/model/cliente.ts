215f84a9b0f5111c9cc32983302b192ba6869fd9
Matheus-Frigola <Matheus.frigola@gmail.com>
2026-03-12 23:08:28 -0300

    Add files via upload

---
+++ src/app/model/cliente.ts
@@ -0,0 +1,8 @@
+export class Cliente {
+    nome: string = "";
+    email: string = "";
+    documento: string = "";
+    telefone: string = "";
+    logradouro: string = "";
+    senha: string = "";
+}
---
+++ src/app/model/contato.spec.ts
@@ -0,0 +1,7 @@
+import { ContatoModel } from './contato';
+
+describe('Contato', () => {
+  it('should create an instance', () => {
+    expect(new ContatoModel()).toBeTruthy();
+  });
+});
